66b0aad85d9d50acd7a00d8b2422ce1dec1dfff9
Magnerz1 <doritoopscorpion@gmail.com>
2024-06-01 15:39:27 -0500

    finishing technical ability section

--- src/JS/app.js
+++ src/JS/app.js
@@ -34,6 +34,78 @@ let jsProgress = setInterval(() => {
         clearInterval(jsProgress);
     }
 }, jsSpeed);
+
+let tsCircularProgress = document.getElementById("ts-progress"),
+    tsProgressValue = document.getElementById("ts-value");
+
+let tsProgressStartValue = 0,
+    tsProgressEndValue = 35,
+    tsSpeed = 15;
+
+let tsProgress = setInterval(() => {
+    tsProgressStartValue++;
+
+    tsProgressValue.textContent = `${tsProgressStartValue}`
+    tsCircularProgress.style.background = `conic-gradient(#00ddffa0 ${tsProgressStartValue * 3.6}deg, #063f4c 0deg)`
+
+    if(tsProgressStartValue == tsProgressEndValue){
+        clearInterval(tsProgress);
+    }
+}, tsSpeed);
+
+let wpCircularProgress = document.getElementById("wp-progress"),
+    wpProgressValue = document.getElementById("wp-value");
+
+let wpProgressStartValue = 0,
+    wpProgressEndValue = 70,
+    wpSpeed = 15;
+
+let wpProgress = setInterval(() => {
+    wpProgressStartValue++;
+
+    wpProgressValue.textContent = `${wpProgressStartValue}`
+    wpCircularProgress.style.background = `conic-gradient(#00ddffa0 ${wpProgressStartValue * 3.6}deg, #063f4c 0deg)`
+
+    if(wpProgressStartValue == wpProgressEndValue){
+        clearInterval(wpProgress);
+    }
+}, wpSpeed);
+
+let sqlCircularProgress = document.getElementById("sql-progress"),
+    sqlProgressValue = document.getElementById("sql-value");
+
+let sqlProgressStartValue = 0,
+    sqlProgressEndValue = 30,
+    sqlSpeed = 10;
+
+let sqlProgress = setInterval(() => {
+    sqlProgressStartValue++;
+
+    sqlProgressValue.textContent = `${sqlProgressStartValue}`
+    sqlCircularProgress.style.background = `conic-gradient(#00ddffa0 ${sqlProgressStartValue * 3.6}deg, #063f4c 0deg)`
+
+    if(sqlProgressStartValue == sqlProgressEndValue){
+        clearInterval(sqlProgress);
+    }
+}, sqlSpeed);
+
+let phCircularProgress = document.getElementById("ph-progress"),
+    phProgressValue = document.getElementById("ph-value");
+
+let phProgressStartValue = 0,
+    phProgressEndValue = 27,
+    phSpeed = 15;
+
+let phProgress = setInterval(() => {
+    phProgressStartValue++;
+    phProgressValue.textContent = `${phProgressStartValue}`
+    phCircularProgress.style.background = `conic-gradient(#00ddffa0 ${phProgressStartValue * 3.6}deg, #063f4c 0deg)`
+
+    if(phProgressStartValue == phProgressEndValue){
+        clearInterval(phProgress);
+    }
+}, phSpeed);
+
 // let htmlCircularProgress = document.getElementById("html-progress"),
 //     htmlProgressValue = document.getElementById("html-value");
 
